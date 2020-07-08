8b87688e919906140936834a6d7c1fc0f4351356
SuelyBarreto <suely@barreto.us>
2020-07-07 18:01:34 -0700

    Created production build for S3 deployment.

---
+++ build/precache-manifest.cef8235e75035c978bb73296109fa4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9beae2f11a06d7cd066bc758d9ac25f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65a7ea94593d58ee7c39",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "d94c80f80b0d1ee9838a",
+    "url": "/static/css/main.53a0d309.chunk.css"
+  },
+  {
+    "revision": "65a7ea94593d58ee7c39",
+    "url": "/static/js/2.8bdb3a3a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.8bdb3a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94c80f80b0d1ee9838a",
+    "url": "/static/js/main.9bc820ec.chunk.js"
+  },
+  {
+    "revision": "586b8ce51bafaafc43a7",
+    "url": "/static/js/runtime-main.8a21c167.js"
+  }
+]);
